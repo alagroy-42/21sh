dc077f793899b496382f1380b3a695ed4fe708d0
Paul-louis CHARRIERE <pcharrie@e2r5p5.42.fr>
2019-10-04 09:34:05 +0200

    cd fix 1

--- libft/ft_2dstrjoin_nodel.c
+++ libft/ft_2dstrjoin_nodel.c
@@ -6,7 +6,7 @@
 /*   By: pcharrie <pcharrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 11:21:03 by pcharrie          #+#    #+#             */
-/*   Updated: 2019/10/03 15:45:09 by pcharrie         ###   ########.fr       */
+/*   Updated: 2019/10/04 09:04:06 by pcharrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ char	**ft_2dstrjoin_nodel(char **tab1, char **tab2)
 	len = ft_2dstrlen(tab1) + ft_2dstrlen(tab2);
 	if (!(new_tab = (char **)malloc((len + 1) * sizeof(char *))))
 		return (NULL);
-	i = -1;
-	j = -1;
-	while (tab1 && tab1[++i])
-		new_tab[++j] = ft_strdup(tab1[i]);
-	i = -1;
-	while (tab2 && tab2[++i])
-		new_tab[++j] = ft_strdup(tab2[i]);
+	i = 0;
+	j = 0;
+	while (tab1 && tab1[i])
+		new_tab[j++] = ft_strdup(tab1[i++]);
+	i = 0;
+	while (tab2 && tab2[i])
+		new_tab[j++] = ft_strdup(tab2[i++]);
 	new_tab[len] = NULL;
 	return (new_tab);
 }
--- srcs/set_pwd_utils.c
+++ srcs/set_pwd_utils.c
@@ -6,7 +6,7 @@
 /*   By: pcharrie <pcharrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 06:19:15 by pcharrie          #+#    #+#             */
-/*   Updated: 2019/10/03 15:44:23 by pcharrie         ###   ########.fr       */
+/*   Updated: 2019/10/04 09:33:34 by pcharrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,22 @@ char	*ft_2dstr_to_path(char **tab1)
 	return (str);
 }
 
+int		ft_2dstrjoin_path_tablen(char **tab1)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (tab1 && tab1[i])
+	{
+		if (ft_strcmp(tab1[i], "..") && ft_strcmp(tab1[i], "."))
+			j++;
+		i++;
+	}
+	return (j);
+}
+
 char	**ft_2dstrjoin_path(char **tab1, char **tab2, int k)
 {
 	char	**new_tab;
@@ -47,17 +63,18 @@ char	**ft_2dstrjoin_path(char **tab1, char **tab2, int k)
 	int		j;
 	int		len;
 
-	len = ft_2dstrlen(tab1) + ft_2dstrlen(tab2);
+	len = ft_2dstrlen(tab1) + ft_2dstrjoin_path_tablen(tab2);
 	if (!(new_tab = (char **)malloc((len + 1) * sizeof(char *))))
 		return (NULL);
-	i = -1;
-	j = -1;
-	while (tab1 && tab1[++i])
-		new_tab[++j] = ft_strdup(tab1[i]);
-	while (tab2 && tab2[k])
+	i = 0;
+	j = 0;
+	while (tab1 && tab1[i])
+		new_tab[j++] = ft_strdup(tab1[i++]);
+	while (tab2 && k < ft_2dstrlen(tab2) && tab2[k])
 	{
 		if (ft_strcmp(tab2[k], "..") && ft_strcmp(tab2[k], "."))
-			new_tab[++j] = ft_strdup(tab2[k++]);
+			new_tab[j++] = ft_strdup(tab2[k]);
+		k++;
 	}
 	new_tab[len] = NULL;
 	return (new_tab);
